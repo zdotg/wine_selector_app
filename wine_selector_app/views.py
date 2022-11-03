99729674cfd2c84a3354c20c8af164e9669399f1
zdotg <zach.ecab@gmail.com>
2022-11-03 15:46:09 -0400

    updated html file name

--- wine_selector_app/views.py
+++ wine_selector_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def wine_request(request):
-    return render(request, 'wineapp/index.html', {})
+    return render(request, 'index.html', {})

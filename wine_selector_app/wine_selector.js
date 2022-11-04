24ff078048d39033a9c4bf50315fd651e616d781
zdotg <zach.ecab@gmail.com>
2022-11-04 17:47:22 -0400

    added variables for wine via ID

---
+++ wine_selector_app/wine_selector.js
@@ -0,0 +1,6 @@
+var red = document.getElementById("red");
+var white = document.getElementById("white");
+var rose = document.getElementById("rose");
+var orange = document.getElementById("orange");
+var petNat = document.getElementById("pet_nat");
+var bubbles = document.getElementById("bubbles");
